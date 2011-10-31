980ae5a8bcfa709f560441c6a7d1405a716a71e3
Michael Turner <michael@turnerendlesslearning.com>
2011-10-31 10:16:42 -0500

    Added GPL 3 License to file headers

--- src/com/turnerendlesslearning/audiobook/MainForm.java
+++ src/com/turnerendlesslearning/audiobook/MainForm.java
@@ -1,23 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * MainForm.java
+ * Copyright (C) 2011 Michael Turner <michael at turnerendlesslearning.com>
  *
- * Created on Feb 8, 2010, 8:16:21 PM
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.turnerendlesslearning.audiobook;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.logging.Level;
